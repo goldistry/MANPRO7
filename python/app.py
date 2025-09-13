f661ff0e3ff668ab42aa5ffe965b46358836cb66
goldistry <goldistry99@gmail.com>
2025-09-13 22:06:42 +0700

    CHORE: delete useless code comments

--- python/app.py
+++ python/app.py
@@ -1,16 +1,14 @@
 # run ini di terminal sebelum run code:
-# pip install pandas openpyxl
+# pip install -r requirements.txt
 import pandas as pd
 import glob
 import os
 
-# Path relatif dari folder script (python) ke folder dataset
+# path relatif dari folder script (python) ke folder dataset
 path_ke_dataset = '../dataset/*.xlsx'
 
-# Gunakan path tersebut untuk menemukan semua file Excel
 file_paths = glob.glob(path_ke_dataset)
 
-# Cek apakah file ditemukan
 if not file_paths:
     print("Tidak ada file Excel (.xlsx) yang ditemukan di folder dataset.")
 else:
@@ -20,30 +18,28 @@ else:
 
     list_of_dataframes = []
 
-    # Loop melalui setiap path file yang ditemukan
+    # loop tiap path file yang ditemukan
     for path in file_paths:
         try:
-            # GUNAKAN pd.read_excel UNTUK FILE .xlsx
             df = pd.read_excel(path, skiprows=4)
             list_of_dataframes.append(df)
         except Exception as e:
             print(f"Error saat membaca file {path}: {e}")
 
-    # Lanjutkan hanya jika berhasil membaca setidaknya satu file
     if list_of_dataframes:
         merged_df = pd.concat(list_of_dataframes, ignore_index=True)
         print("\nSemua file berhasil digabungkan.")
         print("Total baris data setelah digabung:", len(merged_df))
 
-        # Definisikan kolom yang ingin kita proses
+        # define kolom yang mau diambil
         kolom_target = ['Judul', 'ISBN', 'ISBN Dtl.']
         
-        # Cek apakah semua kolom target ada di DataFrame
+        # cek apakah semua kolom target ada di DataFrame
         if all(col in merged_df.columns for col in kolom_target):
-            # Langsung pilih kolom yang dibutuhkan
+            # pilih kolom yang dibutuhkan
             books_df = merged_df[kolom_target]
 
-            # Hapus baris di mana 'Judul' kosong
+            #hapus baris yang 'Judul' kosong
             books_df = books_df.dropna(subset=['Judul'])
             
             # Hapus baris duplikat
@@ -62,7 +58,6 @@ else:
             print("File ini berisi daftar buku unik (Judul, ISBN, ISBN Dtl.).")
         
         else:
-            # Pesan error yang relevan
             print(f"\nError: Satu atau lebih kolom target {kolom_target} tidak ditemukan.")
             print("Pastikan semua file Excel Anda memiliki kolom tersebut.")
             print("Kolom yang tersedia:", merged_df.columns.tolist())
